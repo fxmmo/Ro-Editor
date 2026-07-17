a8b533dc929259ec9269f310c73e44804e4d2154
fxmmo <Jogoroblox86@gmail.com>
2026-07-17 02:05:59 -0300

    update src/modules/create.lua

---
+++ src/modules/create.lua
@@ -0,0 +1,46 @@
+local cloneref = (cloneref or clonereference or function(instance)
+  return instance
+end)
+
+local configs = {
+  BackgroundColor = Color3.fromRGB(43,43,43),
+  BackgroundSize = UDim2.new(0, 300,0, 353),
+  BackgroundPosition = UDim2.new(1, -300, 0, 0),
+  ButtonsColor = Color3.fromRGB(16,68,210),
+  Placehoulders = Color3.fromRGB(46,98,239)
+}
+
+local Create = {}
+Create.__index = Create
+
+function Create.new(instance)
+  local self = setmetatable({}, Create)
+    self.instance = instance
+    return self
+end
+
+function Create:Window()
+  local Screen = Instance.new("ScreenGui")
+  Screen.IgnoreGuiInset = false
+  Screen.ResetOnSpawn = false
+  Screen.Name = "Dex"
+  Screen.Parent = cloneref(game:GetService("CoreGui"))
+  
+  local window = Instance.new("Frame")
+  window.Parent = Screen
+  window.Name = "Window"
+  window.Size = configs.BackgroundSize
+  window.BackgroundColor3 = configs.BackgroundColor
+  window.Position = configs.BackgroundPosition
+
+  local content = Instance.new("Frame")
+  content.Size = UDim2.new(1, 0, 1, -20)
+  content.Position = UDim2.new(0, 0, 0, 20)
+  content.BackgroundColor3 = Color3.fromRGB(40,40,40)
+  content.Name = "Content"
+  content.Parent = window
+  
+  return Screen, window, content
+end
+
+return Create
