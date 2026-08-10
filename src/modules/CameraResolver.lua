ee47a5b9c85c0406f5558893083c98ed5432377f
fxmmo <Jogoroblox86@gmail.com>
2026-08-10 15:39:01 -0300

    update src/modules/CameraResolver.lua

--- src/modules/CameraResolver.lua
+++ src/modules/CameraResolver.lua
@@ -1,7 +1,30 @@
 local module = {}
 
+local _cameraCache = {}
+
+function module.createCam(props)
+  if not props then return end 
+
+  local cam_folder = workspace.cam_folder
+  if not cam_folder then 
+    cam_folder = Instance.new("Folder")
+    cam_folder.Name = "cams_folder"
+  end
+
+  for _, cam in ipairs(cam_folder:GetChildren()) do 
+    if cam and cam:IsA("")
+end
+
 function module.get()
-	return workspace:FindFirstChild("CameraPart") or workspace:FindFirstChildWhichIsA("Part")
+	for _, c in pairs(_cameraCache) do 
+    if c and c.Parent then 
+      local data = {
+        Name = c.Name,
+        CFrame = c.CFrame,
+        Parent = c.Parent
+      }
+      return data 
+    end
 end
 
 return module
